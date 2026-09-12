d40bccde5adfc5013745127e36f5429d0f20863b
TeomanDeniz <te-odeniz@hotmail.com>
2026-09-12 19:23:39 +0300

    2026/09/12

--- lang.js
+++ lang.js
@@ -15,7 +15,7 @@
 var		lang = "tr";
 const	lang_table = {
 	"tr": {
-		0: "Merhaba, ALKAN Vernik & Boya ürünleri hakkında bilgi ve fiyat teklifi almak istiyorum.",
+		0: "Merhaba, Mavi Hırdavat ürünler hakkında bilgi ve fiyat teklifi almak istiyorum.",
 		1: "\n\nAd Soyad: ${name}\nTelefon: ${phone}\nE-posta: ${email}\nÜrün Grubu: ${group}\nMesaj: ${message}"
 	}
 };
